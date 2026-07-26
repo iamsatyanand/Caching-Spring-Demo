e84937baecfd8b5300b1c07659000d5372d48927
iamsatyanand <iamsatyanandmishra@gmail.com>
2026-07-26 12:44:15 +0530

    in memory cache implented

---
+++ src/main/java/com/satyanand/caching_demo_app/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.satyanand.caching_demo_app.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
