303a921c304f7810da586f054f249347ecdb20d6
iamsatyanand <iamsatyanandmishra@gmail.com>
2026-07-26 10:49:52 +0530

    added controllers and global exception handler

--- src/main/java/com/satyanand/caching_demo_app/controllers/EmployeeController.java
+++ src/main/java/com/satyanand/caching_demo_app/controllers/EmployeeController.java
@@ -3,15 +3,41 @@ package com.satyanand.caching_demo_app.controllers;
 import com.satyanand.caching_demo_app.dtos.EmployeeDto;
 import com.satyanand.caching_demo_app.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employees")
 @RequiredArgsConstructor
 public class EmployeeController {
     private final EmployeeService employeeService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(Long id){
+        return ResponseEntity.ok(employeeService.getEmployeeById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createNewEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto createdEmployeeDto = employeeService.createNewEmployee(employeeDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdEmployeeDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable Long id, @RequestBody EmployeeDto employeeDto) {
+        EmployeeDto updatedEmployee = employeeService.updateEmployee(id, employeeDto);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
 
 
 }
